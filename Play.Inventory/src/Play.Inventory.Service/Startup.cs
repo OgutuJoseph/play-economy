140fc63660d82a543e003d2534810db5c0ad32e2
OgutuJoseph <onyango.joseph@yahoo.com>
2023-08-03 11:22:56 +0300

    Implementing synchronous communication via IHttpClientFactory

--- Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -1,17 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Play.Common.MongoDB;
+using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 
 namespace Play.Inventory.Service
@@ -30,6 +25,11 @@ namespace Play.Inventory.Service
         {
             services.AddMongo()
                     .AddMongoRepository<InventoryItem>("inventoryitems");
+
+            services.AddHttpClient<CatalogClient>(client => 
+            {
+                client.BaseAddress = new Uri("https://localhost:5001");
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
