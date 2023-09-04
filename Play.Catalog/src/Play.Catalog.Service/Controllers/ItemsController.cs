1f31eece049b55675826d3c9b71e5a7a43b7b3a9
OgutuJoseph <onyango.joseph@yahoo.com>
2023-09-04 14:08:03 +0300

    Publishing messages via MassTransit

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
+using Play.Catalog.Contracts;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
 using Play.Common;
@@ -21,34 +23,41 @@ namespace Play.Catalog.Service.Controllers
         // };
 
         private readonly IRepository<Item> itemsRepository;
-        private static int requestCounter = 0;
+        // private static int requestCounter = 0;
+        private readonly IPublishEndpoint publishEndpoint;
 
-        public ItemsController(IRepository<Item> itemsRepository)
+        public ItemsController(IRepository<Item> itemsRepository, IPublishEndpoint publishEndpoint)
         {
             this.itemsRepository = itemsRepository;
+            this.publishEndpoint = publishEndpoint;
         }
 
         /** Get all items - async updated */
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ItemDto>>> GetAsync()
         {
-            requestCounter++;
-            Console.WriteLine($"Request {requestCounter}: Starting...");
+            // requestCounter++;
+            // Console.WriteLine($"Request {requestCounter}: Starting...");
 
-            if (requestCounter <= 2){
-                Console.WriteLine($"Request {requestCounter}: Delaying...");
-                await Task.Delay(TimeSpan.FromSeconds(10));
-            }
+            // if (requestCounter <= 2){
+            //     Console.WriteLine($"Request {requestCounter}: Delaying...");
+            //     await Task.Delay(TimeSpan.FromSeconds(10));
+            // }
 
-            if (requestCounter <= 4){
-                Console.WriteLine($"Request {requestCounter}: 500 (Internal Server Error)...");
-                return StatusCode(500);
-            }
+            // if (requestCounter <= 4){
+            //     Console.WriteLine($"Request {requestCounter}: 500 (Internal Server Error)...");
+            //     return StatusCode(500);
+            // }
+
+            // var items = ( await itemsRepository.GetAllAsync())
+            //             .Select(item => item.AsDto());
+
+            // Console.WriteLine($"Request {requestCounter}: 200 (Ok).");
+            // return Ok(items);
 
             var items = ( await itemsRepository.GetAllAsync())
                         .Select(item => item.AsDto());
 
-            Console.WriteLine($"Request {requestCounter}: 200 (Ok).");
             return Ok(items);
         }
 
@@ -80,6 +89,8 @@ namespace Play.Catalog.Service.Controllers
 
             await itemsRepository.CreateAsync(item);
 
+            await publishEndpoint.Publish(new CatalogItemCreated(item.Id, item.Name, item.Description));
+
             return CreatedAtAction(nameof(GetByIdAsync), new { id = item.Id }, item);
         }
 
@@ -99,6 +110,8 @@ namespace Play.Catalog.Service.Controllers
             existingItem.Price = updateItemDto.Price;
 
             await itemsRepository.UpdateAsync(existingItem);
+            
+            await publishEndpoint.Publish(new CatalogItemUpdated(existingItem.Id, existingItem.Name, existingItem.Description));            
 
             return NoContent();
         }
@@ -115,6 +128,8 @@ namespace Play.Catalog.Service.Controllers
             };
 
             await itemsRepository.RemoveAsync(item.Id);
+
+            await publishEndpoint.Publish(new CatalogItemDeleted(id));
 
             return NoContent();
         }
